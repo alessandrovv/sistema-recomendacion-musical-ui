eee6a2c71ce51402d77bae3663427459c04c3e74
Ayrton Soto <ayrton.soto@janaq.com>
2024-01-30 12:54:38 -0500

    Ayrton: ni idea que fue

--- src/app/services/data/user.service.ts
+++ src/app/services/data/user.service.ts
@@ -11,7 +11,6 @@ export class UserService {
   async getPlaylists(){
     let playlists = [];
     await axios.get('http://localhost:3000/playlists/my').then((response: any)=>{
-      console.log('playlists response: ', response);
       playlists = response.data.data;
     }).catch((error)=>{
       console.error('error on getAllSongs: ', error);
